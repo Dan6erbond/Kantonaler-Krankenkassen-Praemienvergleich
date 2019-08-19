d911268870ec1d035746b7b603b0d68a9b7d7d08
Dan6erbond <moravrav@gmail.com>
2019-08-19 19:10:47 +0200

    mobile index site

--- js/index.js
+++ js/index.js
@@ -1,5 +1,24 @@
 $.get('js/cantons.json', function(data) {
    cantons = JSON.parse(data);
+   cantons.sort((a, b) => (a.name > b.name) ? 1 : -1)
+   for (var i in cantons) {
+     canton = cantons[i];
+     /*<a href="canton.html?canton=AG">
+       <li>
+         <img src="img/coat_of_arms/AG.svg" alt="">
+         <span>Kanton Aargau</span>
+       </li>
+     </a>*/
+     var a = $("<a>", {href: "canton.html?canton=" + canton["code"]});
+     var li = $("<li>");
+     var img = $("<img>", {src: "img/coat_of_arms/" + canton["code"] + ".svg"});
+     var span = $("<span>");
+     span.html("Kanton " + canton["name"]);
+     li.append(img);
+     li.append(span);
+     a.append(li);
+     $("#canton_buttons").append(a);
+   }
 }, 'text');
 
 $(document).ready(function() {
